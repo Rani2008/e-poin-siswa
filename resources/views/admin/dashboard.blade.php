ba9a777d6d36062b0216c85d65bbecef084b5326
Rahadirahman21 <rahadi.rahman2007@smkn4-tsm.sch.id>
2024-11-20 10:58:09 +0700

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <a class="nav-link" href="{{route('siswa.index')}}">Data siswa</a>
+  <a href="{{route('logout')}}" onClick="event.preventDefault(); document.getElementById('logout-form').submit();">logout</a>
+    <form id="logout-form" action="{{route('logout')}}" method="POST">
+      @csrf
+
+    </form>
+    <h1>dashboard admin</h1>
+    @if ($massage=Session::get('success'))
+    <p>{{$massage}}</p>
+    @else
+    <p>you are logged in!</p>
+    @endif
+</body>
+</html>
